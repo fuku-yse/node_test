d821a7050680fcb87502091bcf141e54340af94d
fuku-yse <190263ms@yse-c.net>
2020-12-21 12:24:30 +0900

    mysql

--- controllers/HomeController.js
+++
@@ -1,9 +0,0 @@
-exports.index = (req, res) => {
-    let data = { title: 'Index Page' };
-    res.render('home/index.ejs', data)
-}
-
-exports.about = (req, res) => {
-    let data = { title: 'About Page' }
-    res.render('home/about.ejs', data)
-}
